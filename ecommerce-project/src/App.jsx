6a4408db57e553e2013bb8247f50d11bb21effc8
eiwarphyu <eiwarphyuucsm@gmail.com>
2026-02-12 12:43:38 +0700

    Move tracking into React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from './pages/HomePage';
 import './App.css'
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
+import { Tracking } from './pages/Tracking';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route index element={<HomePage />}/>
       <Route path="checkout" element={<CheckoutPage/>}/>
       <Route path="orders" element={<OrdersPage/>}/>
+      <Route path="tracking" element={<Tracking/>}/>
     </Routes>
     
   )
