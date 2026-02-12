4128e26163f915dcaf389e4c0f9b00ce4e707d91
eiwarphyu <eiwarphyuucsm@gmail.com>
2026-02-12 15:09:53 +0700

    Use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,16 +1,15 @@
+import axios from 'axios';
 import { Header } from './../components/Header';
 import './HomePage.css';
 import CheckMark from './../assets/images/icons/checkmark.png';
 import { products } from './../../starting-code/data/products.js';
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response)=>{
-      return response.json();
-    }).then((data)=>{
-      console.log(data);
-    });
-    
+      console.log(response.data);
+    })
+
   return (
     <>
       <title>Ecommerce Project</title>
