5594c8d47dc837342f781ee665d31ddd727b55a3
eiwarphyu <eiwarphyuucsm@gmail.com>
2026-02-12 13:31:25 +0700

    Move image into src and import it

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -1,5 +1,9 @@
 import './CheckoutHeader.css';
 import { Link } from 'react-router';
+import MobileLogo from './../../assets/images/mobile-logo.png';
+import Logo from './../../assets/images/logo.png';
+import CheckoutLockIcon from './../../assets/images/icons/checkout-lock-icon.png';
+
 export function CheckoutHeader(){
   return(
     <>
@@ -7,8 +11,8 @@ export function CheckoutHeader(){
         <div className="header-content">
           <div className="checkout-header-left-section">
             <Link to="/">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
+              <img className="logo" src={Logo} />
+              <img className="mobile-logo" src={MobileLogo} />
             </Link>
           </div>
 
@@ -18,7 +22,7 @@ export function CheckoutHeader(){
           </div>
 
           <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
+            <img src={CheckoutLockIcon} />
           </div>
         </div>
       </div>
